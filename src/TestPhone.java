fe1f6f6e7ef72dfc70a7d8a35f116bef8b1c21cd
speechchemistry <speechchemistry@gmail.com>
2018-06-27 16:44:33 +0100

    standard commit

---
+++ src/TestPhone.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class TestPhone {
+    public static void main(String[] args) throws IOException {
+    	LinkedList<String> bundleList = new LinkedList<String>(Arrays.asList(
+        		"../resources/common/hayes_features_utf8nfc.tsv",
+        		"../resources/common/extra_and_override_phone_features_utf8nfc.tsv",
+        		"../resources/common/extra_auto_generated_utf8nfc.tsv"));
+        String phoneComponentsFilename1 = args[0];
+        PhoneInventory lang1PhInv = new PhoneInventory(phoneComponentsFilename1,bundleList);
+        String phoneComponentsFilename2 = args[1];
+        PhoneInventory lang2PhInv = new PhoneInventory(phoneComponentsFilename2,bundleList);
+
+        List<Phone> phList = new LinkedList<Phone>(lang1PhInv.getPhoneSet());
+        for(Phone lang1Ph:phList){
+        	System.out.println(lang1Ph+"\t"+lang2PhInv.singlePhoneClosest(lang1Ph));
+        }
+    }
+}
